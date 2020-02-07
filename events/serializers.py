bd23846aeb861e1da21f66a2d25957d88f602226
Jay Parmani <jsparmani@gmail.com>
2020-02-08 00:04:03 +0530

    events backend setup complete

--- events/serializers.py
+++ events/serializers.py
@@ -2,11 +2,21 @@ from rest_framework import serializers
 from . import models
 
 
+class TopicSerializer(serializers.ModelSerializer):
+    """Serialize Topic model"""
+
+    class Meta:
+        model = models.Topic
+        fields = ('id', 'name')
+        read_only_fields = ('id', )
+
 class EventSerializer(serializers.ModelSerializer):
     """Serialize Event model"""
 
+    topics = TopicSerializer(read_only=True, many=True)
+
     class Meta:
         model = models.Event
-        fields = ('id', 'date_time',
-                  'venue', 'title', 'link', 'docs', 'headline_event')
+        fields = ('id', 'date','time',
+                  'venue', 'title', 'info','topics', 'link', 'docs', 'headline_event')
         read_only_fields = ('id', )
