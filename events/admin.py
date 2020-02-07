7fe2feaef7dbcd9905d85ccd98f2a8713fc3178a
Jay Parmani <jsparmani@gmail.com>
2020-02-07 22:16:55 +0530

    add topics to the event model

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Event)
+
+class TopicInline(admin.TabularInline):
+    model = models.Event.topics.through
+
+    verbose_name = "Topic"
+    verbose_name_plural = "Topics"
+
+    def get_queryset(self, request):
+        qs = super(TopicInline, self).get_queryset(request)
+        return qs
+
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [
+        TopicInline,
+    ]
+    exclude = ('topics',)
+
+
+admin.site.register(models.Event, EventAdmin)
+admin.site.register(models.Topic)
--- events/models.py
+++ events/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
 
+class Topic(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
 class Event(models.Model):
 
     date_time = models.DateTimeField()
     venue = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
-    link = models.URLField()
-    docs = models.URLField()
+    topics = models.ManyToManyField(
+        Topic, related_name='topics', blank=True)
+    link = models.URLField(blank=True)
+    docs = models.URLField(blank=True)
     headline_event = models.BooleanField(default=False)
 
     def __str__(self):
