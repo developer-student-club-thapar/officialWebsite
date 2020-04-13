80c5fa36003a504d4ae6ac61d2ff8da872fad6b4
shubhank-saxena <saxena.shubhank.19@gmail.com>
2020-04-13 22:14:15 +0530

    fixed manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.DSCBackend.settings')
+    os.environ["DJANGO_SETTINGS_MODULE"] = "backend.DSCBackend.settings"
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
