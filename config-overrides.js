d535ac5c129be5d4f7f67acf1f4329fd5d58d786
shubhank-saxena <saxena.shubhank.19@gmail.com>
2020-04-15 14:26:37 +0530

    fixed lint

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,11 @@
 const path = require("path");
 
 module.exports = {
-    paths: function (paths, env) {
-        paths.appIndexHtml = path.resolve(
-            __dirname,
-            "Frontend/public/index.html"
-        );
-        paths.appBuild = path.resolve(__dirname, "Frontend/build");
-        paths.appIndexJs = path.resolve(__dirname, "Frontend/src/index.js");
-        paths.appSrc = path.resolve(__dirname, "Frontend/src");
-        return paths;
-    },
+  paths: function(paths, env) {
+    paths.appIndexHtml = path.resolve(__dirname, "Frontend/public/index.html");
+    paths.appBuild = path.resolve(__dirname, "Frontend/build");
+    paths.appIndexJs = path.resolve(__dirname, "Frontend/src/index.js");
+    paths.appSrc = path.resolve(__dirname, "Frontend/src");
+    return paths;
+  }
 };
