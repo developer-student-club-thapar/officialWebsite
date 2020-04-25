2d6e44db3918e89949f2e9ada52691855b36b9ed
Jay Parmani <jsparmani@gmail.com>
2020-04-25 18:12:30 +0530

    add project lead to serializer

--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -7,8 +7,9 @@ class ProjectSerializer(serializers.ModelSerializer):
     """Serialize Project model"""
 
     members = MemberSerializer(read_only=True, many=True)
+    project_lead = MemberSerializer(read_only=True)
 
     class Meta:
         model = models.Project
-        fields = ('id', 'name', 'description', 'members', 'github_link', 'funding', 'faculty', 'extra')
+        fields = ('id', 'name', 'project_lead', 'description', 'members', 'github_link', 'funding', 'faculty', 'extra')
         read_only_fields = ('id',)
