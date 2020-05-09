c39fc3f38be5b1dc4416a04363cc4629aa01dc21
Jay Parmani <jsparmani@gmail.com>
2020-05-09 21:36:57 +0530

    run pre-commit

--- Frontend/src/App.test.js
+++ Frontend/src/App.test.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import { shallow, mount } from 'enzyme';
+import { shallow, mount } from "enzyme";
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
