4e9b82644450ca808d7ba57f2c85fe63a242d19b
Shreshth Arora <sarora_be18@thapar.edu>
2020-05-15 14:14:24 +0530

    podcast logic backend

---
+++ backend/podcast/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import PodcastGuest, PodcastSeries, Podcast, PodcastLink
+# Register your models here.
+
+admin.site.register(PodcastGuest)
+admin.site.register(PodcastSeries)
+admin.site.register(Podcast)
+admin.site.register(PodcastLink)
