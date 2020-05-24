c1499cff9e4979b0048e575dc04e88cb7bdf8db7
Jay Parmani <jsparmani@gmail.com>
2020-05-24 12:30:46 +0530

    folder rename

---
+++ officialWebsite/projects/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from . import models
+from officialWebsite.members.serializers import MemberSerializer
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    """Serialize Project model"""
+
+    members = MemberSerializer(read_only=True, many=True)
+    project_lead = MemberSerializer(read_only=True)
+    image = serializers.ImageField()
+
+    class Meta:
+        model = models.Project
+        fields = ('id', 'name', 'project_lead', 'image', 'description', 'members', 'github_link', 'funding', 'faculty', 'extra')
+        read_only_fields = ('id',)
