cb5048fadefc3bcb2be8b83eab6fdd0f688e9751
Jay Parmani <jsparmani@gmail.com>
2020-06-04 17:53:52 +0530

    initial pm2 setup

---
+++ deploy/deploy.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  deploy: {
+    production: {
+      key: "./dsc-deploy.ppk",
+      user: "dsc-deploy",
+      host: ["35.213.186.79"],
+      ref: "origin/master",
+      repo: "https://github.com/developer-student-club-thapar/officialWebsite",
+      path: "/home/dsc-deploy/officialWebsite-production",
+      "post-setup":
+        "pip3 install poetry && poetry install && python manage.py migrate",
+      "post-deploy":
+        "authbind --deep pm2 startOrRestart ./deploy/production.config.js"
+    }
+  }
+};
