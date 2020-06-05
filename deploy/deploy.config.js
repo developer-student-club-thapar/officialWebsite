e0d21911d75c8022959fe181d2bdf181af935c0c
Jay Parmani <jsparmani@gmail.com>
2020-06-05 20:00:52 +0530

    linting

--- deploy/deploy.config.js
+++ deploy/deploy.config.js
@@ -10,8 +10,7 @@ module.exports = {
       path: "/home/ubuntu/officialWebsite-production",
       "post-setup":
         "sudo pip3.7 install poetry && poetry install && python3.7 manage.py migrate 0.0.0.0:8000",
-      "post-deploy":
-        "pm2 startOrRestart ./deploy/production.config.js"
+      "post-deploy": "pm2 startOrRestart ./deploy/production.config.js"
     }
   }
 };
