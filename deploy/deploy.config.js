16f26a1aaf83ec4193082282d4dd494ef096d96b
Jaskeerat Singh Randhawa <jaskee789@gmail.com>
2020-06-09 00:24:04 +0530

    rearrange migration command

--- deploy/deploy.config.js
+++ deploy/deploy.config.js
@@ -9,9 +9,9 @@ module.exports = {
         "https://github.com/developer-student-club-thapar/officialWebsite.git",
       path: "/home/ubuntu/officialWebsite-production",
       "post-setup":
-        "sudo pip3.7 install poetry && poetry install && python3.7 manage.py migrate 0.0.0.0:8000",
+        "sudo pip3.7 install poetry && poetry install",
       "post-deploy":
-        "source env/bin/activate && gunicorn officialWebsite.config.wsgi --bind 0.0.0.0:8000 -D"
+        "source env/bin/activate && python3.7 manage.py migrate && gunicorn officialWebsite.config.wsgi --bind 0.0.0.0:8000 -D"
     }
   }
 };
