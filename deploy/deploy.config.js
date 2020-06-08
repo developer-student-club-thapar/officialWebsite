7ea1a6c313d21dcf9abe2b265a8ff938ccb5582f
Jaskeerat Singh Randhawa <jaskee789@gmail.com>
2020-06-09 00:19:37 +0530

    pm2 confug

--- deploy/deploy.config.js
+++ deploy/deploy.config.js
@@ -10,7 +10,8 @@ module.exports = {
       path: "/home/ubuntu/officialWebsite-production",
       "post-setup":
         "sudo pip3.7 install poetry && poetry install && python3.7 manage.py migrate 0.0.0.0:8000",
-      "post-deploy": "pm2 startOrRestart ./deploy/production.config.js"
+      "post-deploy":
+        "source env/bin/activate && gunicorn officialWebsite.config.wsgi --bind 0.0.0.0:8000 -D"
     }
   }
 };
