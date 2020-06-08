3d5749ff15e35cd3b75b6371ea4695a0d94a8a40
Jay Parmani <jsparmani@gmail.com>
2020-06-09 01:25:58 +0530

    update host

--- deploy/deploy.config.js
+++ deploy/deploy.config.js
@@ -3,13 +3,12 @@ module.exports = {
     production: {
       key: "./deploy/ubuntu",
       user: "ubuntu",
-      host: ["library.dsctiet.tech"],
+      host: ["api.dsctiet.tech"],
       ref: "origin/master",
       repo:
         "https://github.com/developer-student-club-thapar/officialWebsite.git",
       path: "/home/ubuntu/officialWebsite-production",
-      "post-setup":
-        "sudo pip3.7 install poetry && poetry install",
+      "post-setup": "sudo pip3.7 install poetry && poetry install",
       "post-deploy":
         "source env/bin/activate && python3.7 manage.py migrate && gunicorn officialWebsite.config.wsgi --bind 0.0.0.0:8000 -D"
     }
