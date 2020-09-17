63674da6c241e75eff264415fda3db71e70f243b
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2020-09-17 12:28:45 +0530

    Endpoint for archive feature ready

--- officialWebsite/team/models.py
+++ officialWebsite/team/models.py
@@ -6,7 +6,6 @@ from officialWebsite.members.models import Member
 class Team(models.Model):
 
     name = models.CharField(max_length=255)
-    heads = models.ManyToManyField(Member, related_name="heads", blank=True)
     members = models.ManyToManyField(Member, related_name="members", blank=True)
 
     def __str__(self):
--- officialWebsite/team/views.py
+++ officialWebsite/team/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from . import models
 from . import serializers
 from rest_framework import viewsets, status, mixins, generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class TeamViewset(generics.ListAPIView):
@@ -9,3 +11,11 @@ class TeamViewset(generics.ListAPIView):
 
     serializer_class = serializers.TeamSerializer
     queryset = models.Team.objects.all()
+
+
+class Archive(APIView):
+    def get(self, request, year, *args, **kwargs):
+        print(year)
+        Team = models.Team.objects.get(name=year)
+        team_serializer = serializers.TeamSerializer(Team)
+        return Response(team_serializer.data)
