cdfc949030618122cc6988d1570140d3ec6b8864
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-20 14:44:00 +0000

    Deploy developer-student-club-thapar/officialWebsite to github.com/developer-student-club-thapar/officialWebsite.git:gh-pages

---
+++ precache-manifest.bc4714c5eee83763ac5685eed1e465f6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101b75076c14c4b9ee14463c0e737e3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12817a2b1d5afd66843b",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "650360c1233d83167ac8",
+    "url": "/static/css/main.16830d08.chunk.css"
+  },
+  {
+    "revision": "12817a2b1d5afd66843b",
+    "url": "/static/js/2.7663bb4c.chunk.js"
+  },
+  {
+    "revision": "6961fc2c84cba0ccdd1b16845a53285c",
+    "url": "/static/js/2.7663bb4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650360c1233d83167ac8",
+    "url": "/static/js/main.566ccc36.chunk.js"
+  },
+  {
+    "revision": "6eaca34e982eaee65e1d",
+    "url": "/static/js/runtime-main.6417d7bf.js"
+  },
+  {
+    "revision": "c94465a39da6b57756a635293c2d86dc",
+    "url": "/static/media/Projects.c94465a3.svg"
+  },
+  {
+    "revision": "6436f6fd9de74263a4b04ecf693e5759",
+    "url": "/static/media/Sponsership.6436f6fd.svg"
+  },
+  {
+    "revision": "c7f8f0025f585bbf8abebfeae1a50cfe",
+    "url": "/static/media/ThaparUniversity_1.c7f8f002.jpg"
+  },
+  {
+    "revision": "2db4b37b64d606d1a619e243e5313b5d",
+    "url": "/static/media/day-mode.2db4b37b.svg"
+  },
+  {
+    "revision": "0d517ed634ef717d1e40123b3af77c11",
+    "url": "/static/media/dsc_logo.0d517ed6.png"
+  },
+  {
+    "revision": "985f281c56fbd532a69b4d448ec926a1",
+    "url": "/static/media/events.985f281c.svg"
+  },
+  {
+    "revision": "b6e2ce1534e3527d7f96b2dd03a1b05d",
+    "url": "/static/media/header.b6e2ce15.png"
+  },
+  {
+    "revision": "8ea92c5af8e32ca009df2270a53cad7a",
+    "url": "/static/media/headerdark.8ea92c5a.png"
+  },
+  {
+    "revision": "547320f25e02a2f4a95668cfab67e044",
+    "url": "/static/media/marker.547320f2.png"
+  },
+  {
+    "revision": "bb132e84a4d6b4b75c15e17f66881163",
+    "url": "/static/media/night-mode.bb132e84.svg"
+  },
+  {
+    "revision": "c557ab2812f206956d2e66e9ef2dcdb3",
+    "url": "/static/media/projects.c557ab28.jpg"
+  },
+  {
+    "revision": "31c7575f4131ca3efbee3e6137f9cf93",
+    "url": "/static/media/team.31c7575f.webp"
+  },
+  {
+    "revision": "f1347d6d74e36958a2226ef418453453",
+    "url": "/static/media/undraw_positive_attitude_xaae.f1347d6d.svg"
+  },
+  {
+    "revision": "fd1f60938ba1426cc566dbcdcb80b216",
+    "url": "/static/media/undraw_scrum_board_cesn.fd1f6093.svg"
+  },
+  {
+    "revision": "20c529007a55005e6726027bc7997229",
+    "url": "/static/media/unnamed.20c52900.png"
+  }
+]);
