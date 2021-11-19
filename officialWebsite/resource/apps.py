e8eda8c0f6c658961d59b7977a8c9c8ddbc61f78
Madlad <72143774+Sahaj-Srivastava24@users.noreply.github.com>
2021-11-20 03:10:54 +0530

    app names changed
    Signed-off-by: Madlad <72143774+Sahaj-Srivastava24@users.noreply.github.com>
    Signed-off-by: Mahima <kulsmahima@gmail.com>

--- officialWebsite/resource/apps.py
+++ officialWebsite/resource/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ResourceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'resource'
+    name = 'officialWebsite.resource'
