18959f63347d8bc6a08e5bc0adc14f0abd03488d
Madlad <72143774+Sahaj-Srivastava24@users.noreply.github.com>
2021-11-26 01:05:52 +0530

    tests done
    Signed-off-by: Madlad <72143774+Sahaj-Srivastava24@users.noreply.github.com>

--- officialWebsite/team/tests.py
+++ officialWebsite/team/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from officialWebsite.team.models import team
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+def create_team():
+    return team.objects.create(
+        name="Test Team",
+    )
+
+
+def create_user():
+    return get_user_model().objects.create_user(name="Test User", email="test@xyz.com")
+
+
+class TestTeamModel(TestCase):
+    def setUp(self):
+        team_new = create_team()
+        user_new = create_user()
+        team_new.user.add(user_new)
+        team_new.save()
+        self.team_new = team_new
+
+    def test_team_creation(self):
+        """
+        Test Topic creation
+        """
+        # topic_new = create_team()
+        self.assertEqual(self.team_new.name, "Test Team")
