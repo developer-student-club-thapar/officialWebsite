ee45a628dcb0f72459e296b3ee0213687eb6a2e3
rdotjain <rupanshijain45678@gmail.com>
2021-11-30 01:22:51 +0530

    Added endpoints for user, project, achievements models
    Signed-off-by: rdotjain <rupanshijain45678@gmail.com>

--- officialWebsite/users/views.py
+++ officialWebsite/users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+from officialWebsite.users.models import User
+from officialWebsite.users.serializers import UserSerializer
 
-# Create your views here.
+
+class LeadListView(APIView):
+    """List all leads"""
+
+    def get(self, request, format=None):
+        leads = User.objects.all().filter(role__iexact="Lead")
+        serializer = UserSerializer(leads, many=True)
+        return Response(serializer.data)
+
+
+class CoLeadListView(APIView):
+    """List all leads"""
+
+    def get(self, request, format=None):
+        leads = User.objects.all().filter(role__icontains="Co-Lead")
+        serializer = UserSerializer(leads, many=True)
+        return Response(serializer.data)
+
+
+class UserViewset(generics.ListAPIView):
+    """Manage members in the database"""
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all().filter(role__icontains="Core")
