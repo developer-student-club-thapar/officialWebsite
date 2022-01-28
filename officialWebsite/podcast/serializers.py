cc08c2ba6568ea73cdb7e5db86fb4c522145a1b2
Raghav Sharma <raghav_tinker@yahoo.com>
2022-01-28 10:20:32 +0530

    minor tweaks in official website
    Signed-off-by: Raghav Sharma <raghav_tinker@yahoo.com>

--- officialWebsite/podcast/serializers.py
+++ officialWebsite/podcast/serializers.py
@@ -2,27 +2,30 @@ from . import models
 from rest_framework import serializers
 
 class PodcastSeriesSerializer(serializers.ModelSerializer):
+    # series serializer
     class Meta:
         model = models.PodcastSeries
         fields = ('id', 'name', 'hosted', 'note', 'logo')
         read_only_fields = ('id',)
-
-class PodcastGuestModelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.PodcastGuest
-        fields = ('id', 'name', 'organisation', 'about', 'image')
-        read_only_fields = ('id',)
-    
-class PodcastGuestSerializer(serializers.ModelSerializer):
-    guest = PodcastGuestModelSerializer(read_only=True)
+class PodcastGuestLinkSerializer(serializers.ModelSerializer):
+    # This serializer is for guest link
     class Meta:
         model = models.PodcastGuestLink
-        fields = ('id', 'guest', 'link', 'link_type')
+        fields = ('id', 'link', 'link_type')
+        read_only_fields = ('id',)
+        
+class PodcastGuestModelSerializer(serializers.ModelSerializer):
+    # This is podcast model serializer.
+    # guest links
+    links = PodcastGuestLinkSerializer(many=True, read_only=True)
+    class Meta:
+        model = models.PodcastGuest
+        fields = ('id', 'name', 'organisation', 'about', 'image', 'links')
         read_only_fields = ('id',)
 
 class PodcastSerializer(serializers.ModelSerializer):
     series = PodcastSeriesSerializer(read_only=True)
-    guest = PodcastGuestSerializer(read_only=True)
+    guest = PodcastGuestModelSerializer(read_only=True)
     class Meta:
         model = models.Podcast
         fields = ('id', 'number', 'guest', 'series', 'recorded_on', 'date_created', 'published', 'image', 'link')
