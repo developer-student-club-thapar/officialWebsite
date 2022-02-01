f97adc17fb80826b88e6dac21aca0eceb8ca0107
Raghav Sharma <raghav_tinker@yahoo.com>
2022-02-01 18:38:17 +0530

    cors error fixed
    Signed-off-by: Raghav Sharma <raghav_tinker@yahoo.com>

--- officialWebsite/config/settings.py
+++ officialWebsite/config/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
     # Third-party apps
     "rest_framework",
+    'corsheaders',
     
     # Backend Apps
     "officialWebsite.misc",
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
