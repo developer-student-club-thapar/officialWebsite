986422c5dd1040ad35df9daf66386532143a8582
Raghav Sharma <raghav_tinker@yahoo.com>
2022-02-02 19:14:00 +0530

    containerised the website
    Signed-off-by: Raghav Sharma <raghav_tinker@yahoo.com>

---
+++ run.sh
@@ -0,0 +1,3 @@
+python manage.py migrate
+python manage.py collectstatic
+gunicorn  --worker-class gevent --certfile=cert.pem --keyfile=privkey.pem --bind 0.0.0.0:8000 officialWebsite.config.wsgi:application
