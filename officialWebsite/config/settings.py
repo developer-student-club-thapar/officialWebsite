28f73bc457e9e95d4d32ea8dfa10b76bac5014f2
Raghav Sharma <raghav_tinker@yahoo.com>
2022-02-02 19:00:39 +0530

    minor fixes in database urls and gettext_lazy
    Signed-off-by: Raghav Sharma <raghav_tinker@yahoo.com>

--- officialWebsite/config/settings.py
+++ officialWebsite/config/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "officialWebsite.config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": env("DATABASE_URL", default=BASE_DIR / "db.sqlite3"),
     }
 }
 
