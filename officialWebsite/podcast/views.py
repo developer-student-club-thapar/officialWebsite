851bbc89fc97be9094984282d8925dee09c6bd39
Raghav Sharma <raghav_tinker@yahoo.com>
2022-04-17 18:07:54 +0530

    views for podcast series added

--- officialWebsite/podcast/views.py
+++ officialWebsite/podcast/views.py
@@ -19,3 +19,11 @@ class PodcastViewset(APIView):
         podcasts = models.Podcast.objects.all()
         serializer = serializers.PodcastSerializer(podcasts, many=True)
         return Response(serializer.data)
+
+# class podcast series
+class PodcastSeriesViewset(APIView):
+    """Manage Podcast Series in the database"""
+    def get(self, request, format=None):
+        series = models.PodcastSeries.objects.all()
+        serializer = serializers.PodcastSeriesSerializer(series, many=True)
+        return Response(serializer.data)
