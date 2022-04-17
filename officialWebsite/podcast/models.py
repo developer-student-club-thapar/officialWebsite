4ebcc84d13cf31c7bfbcf7e50a73ec8d3923db37
Raghav Sharma <raghav_tinker@yahoo.com>
2022-04-17 19:13:29 +0530

    null removed

--- officialWebsite/podcast/models.py
+++ officialWebsite/podcast/models.py
@@ -81,7 +81,7 @@ class Podcast(models.Model):
 
     number = models.IntegerField(editable=False)
     name = models.CharField(max_length=255, default="Podcast Number {}".format(number), null=True, blank=True)
-    guest = models.ForeignKey(PodcastGuest, on_delete=models.PROTECT, null=True, blank=True)
+    guest = models.ForeignKey(PodcastGuest, on_delete=models.PROTECT, blank=True)
     members = models.ManyToManyField(User, blank=True)
     series = models.ForeignKey(PodcastSeries, on_delete=models.PROTECT)
     recorded_on = models.DateTimeField(null = True)
