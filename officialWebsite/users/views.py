9ad34b0e35a7d7921531dfe2aff9f96b1106cc8e
Raghav Sharma <raghav_tinker@yahoo.com>
2022-04-18 18:16:29 +0000

    added members archival for website

--- officialWebsite/users/views.py
+++ officialWebsite/users/views.py
@@ -4,7 +4,6 @@ from rest_framework import generics
 from officialWebsite.users.models import User, Year
 from officialWebsite.users.serializers import UserSerializer
 
-
 class LeadListView(APIView):
     """List all leads"""
 
@@ -46,7 +45,40 @@ class MentorListView(APIView):
         serializer = UserSerializer(mentors, many=True)
         return Response(serializer.data)
 
-# create user
+class YearListView(APIView):
+    """List all the years"""
+    def get(self, request, format=None):
+        years = Year.objects.all().order_by('-year')
+        data = []
+        for year in years:
+            data.append(year.year)
+        return Response(data)
+
+class YearWiseMembersListView(APIView):
+    """List all the years"""
+    # get the year from the url
+    def get(self, request, year, format=None):
+        print(year)
+        year = Year.objects.get(year=year)
+        # get leads of the year
+        members = []
+        leads = User.objects.all().filter(year=year, role__iexact="Lead").order_by('name')
+        serializer = UserSerializer(leads, many=True)
+        members.append({"Lead": serializer.data})
+        # get co-leads of the year
+        co_leads = User.objects.all().filter(year=year, role__iexact="Co-Lead").order_by("name")
+        serializer = UserSerializer(co_leads, many=True)
+        members.append({"Co-Lead": serializer.data})
+        # get mentors of the year
+        mentors = User.objects.all().filter(year=year, role__iexact="Mentor").order_by("name")
+        serializer = UserSerializer(mentors, many=True)
+        members.append({"Mentor": serializer.data})
+        # get core of the year
+        core = User.objects.all().filter(year=year, role__iexact="Core").order_by("name")
+        serializer = UserSerializer(core, many=True)
+        members.append({"Core": serializer.data})
+        return Response(members)
+
 class UserCreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
