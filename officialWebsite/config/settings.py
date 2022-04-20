49b52b61f2f8400c2a3a9700884354be9675d0dd
Raghav Sharma <raghav_tinker@yahoo.com>
2022-04-21 03:20:17 +0530

    glitch in default authentication classes

--- officialWebsite/config/settings.py
+++ officialWebsite/config/settings.py
@@ -63,9 +63,6 @@ REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': (
        'rest_framework.authentication.TokenAuthentication',
    ),
-   'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser'
-   ),
 }
 ROOT_URLCONF = "officialWebsite.config.urls"
 
