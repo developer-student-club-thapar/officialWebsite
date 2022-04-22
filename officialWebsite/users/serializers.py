4fc9728adcfd658411b8eb5ca75657197f12bb8e
Raghav Sharma <raghav_tinker@yahoo.com>
2022-04-23 01:54:04 +0530

    imporved user serializer

--- officialWebsite/users/serializers.py
+++ officialWebsite/users/serializers.py
@@ -1,10 +1,19 @@
 from . import models
 from rest_framework import serializers
 
+
+class YearSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Year
+        fields = ('year',)
 class UserSerializer(serializers.ModelSerializer):
     """Serialize User model"""
 
     image = serializers.ImageField()
+    years = serializers.SerializerMethodField('get_tenure_list')
+
+    def get_tenure_list(self, obj):
+        return [year.year for year in obj.years.all()]
     # get only one year
     class Meta:
         model = models.User
@@ -19,5 +28,6 @@ class UserSerializer(serializers.ModelSerializer):
             "medium_url",
             "dev_url",
             "image",
+            "years", 
         )
         read_only_fields = ("id", )
